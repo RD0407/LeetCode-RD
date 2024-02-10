a5708626445ecec1bc397039d3423ca04daf46c1
Rohaan Deb <67634136+RD0407@users.noreply.github.com>
2024-02-10 22:23:42 +0100

    Time: 184 ms (85.8%), Space: 144.9 MB (76.32%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -16,6 +16,7 @@ public:
      if(!root ) return 0;
      if (root->left == nullptr && root->right == nullptr) return 1;
      if (root->left ==nullptr) return 1+minDepth(root->right);
+     
      if (root->right ==nullptr) return 1+minDepth(root->left);
       return 1+min(minDepth(root->right), minDepth(root->left));
      }
